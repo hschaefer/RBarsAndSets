7881aa73424700a6a5761a42259c289e7bd1cbc7
Heinrich Schaefer <schaefer.heinrich@googlemail.com>
2018-10-03 15:41:14 +0200

    Initial documentation

--- R/BarsAndSets.R
+++ R/BarsAndSets.R
@@ -1,13 +1,28 @@
+#' Plot omics data along with functional classification
+#'
+#' This function plots a dataset (e.g. gene expression ratios) as a bar plot along with
+#' one or multiple tracks indicating the membership in functional or regulatory categories.
+#'
+#' Expression data is given in the first argument as a vector of ratios. Note that the data
+#' should be log transformed and ordered ascending for best results.
+#'
+#' Functional categories are given as a dataframe a column of TRUE/FALSE values for each set.
+#' Column names of the data frame are used as labels in the plot.
+#'
+#'
+#' @param bar_values log transformed, ordered vector of ratios.
+#' @param categories Dataframe with T/F values indicating the membership in categories
+#' and column names as labels.
 #' @export
-BarsAndSetsPlot <- function(bar_values, set_df) {
+BarsAndSetsPlot <- function(bar_values, categories) {
     values = bar_values
     nmeasures = length(values)
-    sets = set_df
+    sets = categories
     nsets = dim(sets)[2]
     set_names = colnames(sets)
     colors = RColorBrewer::brewer.pal(n=nsets, name="Set1")
     ylabel = "log2 fold change"
-    
+
     font_size = 12
     left_margin = max(grid::unit(graphics::strwidth(set_names, font = 12, units = 'in'),"inches")) + grid::unit(2,"lines")
     right_margin = grid::unit(1,"lines")
@@ -17,14 +32,14 @@ BarsAndSetsPlot <- function(bar_values, set_df) {
     set_spacing = grid::unit(0.1,"lines")
     set_margin = grid::unit(0.5,"lines")
     set_area_height = nsets * set_height + (nsets+1)*set_spacing + 2*set_margin
-    
-    
-    
+
+
+
     # Setup plot layout
     grid::grid.newpage()
     grid::pushViewport(grid::viewport(layout = grid::grid.layout(2, 1, heights = grid::unit.c(grid::unit(1,"null"), set_area_height))))
     grid::pushViewport(grid::viewport(layout.pos.row = 1))
-    
+
     # upper part of the plot
     #pushViewport(plotViewport(c(0, 5, 1, 1), yscale = yscale)) # bottom, left, top, right
     grid::pushViewport(grid::viewport(x = left_margin,
@@ -32,8 +47,8 @@ BarsAndSetsPlot <- function(bar_values, set_df) {
                           width = grid::unit(1, "native") - left_margin -right_margin,
                           height = grid::unit(1, "native") - grid::unit(1, "line"),
                           just = c("left", "bottom"),
-                          yscale = yscale)) 
-    
+                          yscale = yscale))
+
     grid::grid.yaxis()
     grid::grid.text(ylabel, x = grid::unit(-3, "lines"), rot = 90)
     grid::grid.rect(x = grid::unit(c(0:(nmeasures-1))/nmeasures, "native"),
@@ -42,7 +57,7 @@ BarsAndSetsPlot <- function(bar_values, set_df) {
               width = grid::unit(frac_width, "native"),
               gp = grid::gpar(fill = "#808080", lty=0),
               just = c(0,0))
-    
+
     grid::popViewport()
     grid::popViewport()
     # lower part of the plot
@@ -59,18 +74,18 @@ BarsAndSetsPlot <- function(bar_values, set_df) {
                             height=set_height,
                             just = c("left", "bottom")
       ))
-      
+
       grid::grid.rect(x = grid::unit(c(0:(nmeasures-1))/nmeasures, "native"),
                 y = rep(grid::unit(0, "native"),nmeasures),
                 height = grid::unit(1, "native"),
                 width = grid::unit(frac_width, "native"),
                 gp = grid::gpar(fill = colors[i], lty=0, alpha = as.numeric(t(sets[,i]))),
                 just = c(0,0))
-      
+
       grid::popViewport()
     }
-    
-    
+
+
     grid::popViewport() #lower viewport
     grid::popViewport() # main
 
